dbd5cdbb451487a2277e15eec4e47ee079a7f013
alekseevpavel04 <48567496+alekseevpavel04@users.noreply.github.com>
2024-02-28 21:43:05 +0300

    Update FastAPI_app.py

--- FastAPI_app.py
+++ FastAPI_app.py
@@ -138,3 +138,34 @@ def predict(data: DataRequest):
         raise HTTPException(status_code=400, detail=str(e))
 
 #uvicorn app:app --reload
+
+"""
+Это скрипт запроса request.py для приложения на FastAPI в файле app.py
+
+
+
+import requests
+
+# Данные для отправки на сервер
+data = {
+    "ticker": "AAPL",
+    "start_date": "2000-01-01",
+    "end_date": "2023-01-01",
+    "split_date": "2022-01-01",
+    "model_choice": "Prophet"  # Выберите модель: "Prophet", "Simple Exponential Smoothing", "ARIMA"
+}
+
+# URL сервера FastAPI
+url = 'http://127.0.0.1:8000/predict'
+
+# Отправляем POST запрос
+response = requests.post(url, json=data)
+
+# Выводим результаты запроса
+if response.status_code == 200:
+    result = response.json()
+    print("Прогнозируемые значения:", result['forecast'])
+    print("MAPE:", result['mape'])
+else:
+    print("Ошибка при выполнении запроса:", response.status_code)
+"""
